6a475ae32e8eec7a67644e125db9a1162decfbad
bartbal <Pioneer12>
2018-11-19 13:18:49 +0100

    opdrachten naar map verplaatst

---
+++ week1/w1_o2_getNumbers.py
@@ -0,0 +1,29 @@
+"""
+description
+this function extracts all numbers from a string. the numbers get grouped if they are separated with a char
+
+parameter
+------
+a : string
+    this is the string where the function searches in
+
+return
+------
+numbers : list
+    this function returns a list with the numbers it found
+
+"""
+def getNumbers(a):
+    assert (type(a) == str), "a is not a string"
+    numbers = []
+    temp = ''
+    for i in a:
+        if(i > '0' and i < '9'):
+            temp += i;
+        elif(temp != ''):
+            numbers.append(int(temp))
+            temp = ''
+    return numbers
+
+print(getNumbers("halli123hoegaat1315het"))
+# print(getNumbers([1, 4, 6]))
